27aff515729b4e70d586a948f15897ee5df84d08
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-19 19:10:53 +0530

    day1

---
+++ helloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    cout << "hello world!" << "\n"; // Inserts a newline
+    cout << "hello world!" << endl; // Inserts a newline and flushes the output buffer
+    cout << "hello world!";
+    return 0;
+}
