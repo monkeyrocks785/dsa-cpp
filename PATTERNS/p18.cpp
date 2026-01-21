c426e8b4ecdbf279a99d526bc76b456e5db60bc3
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-21 22:03:40 +0530

    day3

---
+++ PATTERNS/p18.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        for (char a = 'E' - i; a <= 'E'; a++)
+        {
+            cout << a << " ";
+        }
+        
+        cout << endl;
+    }
+
+    return 0;
+}
