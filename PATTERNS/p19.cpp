abc1b4462f15fde9834856611abfa7690fe7ae5e
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-24 20:42:54 +0530

    day3

---
+++ PATTERNS/p19.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        if (i < n / 2)
+        {
+            for (int j = 0; j < ((n / 2) - i); j++)
+            {
+                cout << "* ";
+            }
+            for (int j = 0; j < 2 * i; j++)
+            {
+                cout << "  ";
+            }
+            for (int j = 0; j < ((n / 2) - i); j++)
+            {
+                cout << "* ";
+            }
+        }
+        else
+        {
+            for (int j = 0; j < (i - n / 2) + 1; j++)
+            {
+                cout << "* ";
+            }
+            for (int j = 0; j < 2 * (n - i - 1); j++)
+            {
+                cout << "  ";
+            }
+            for (int j = 0; j < (i - n / 2) + 1; j++)
+            {
+                cout << "* ";
+            }
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
