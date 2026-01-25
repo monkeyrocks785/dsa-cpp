a9387c1800b3015a711374acad4c15b17838289f
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-25 22:50:36 +0530

    day4

--- gcd_problem.cpp
+++ gcd_problem.cpp
@@ -8,14 +8,25 @@ int main()
     int n1, n2;
     cin >> n1 >> n2;
 
-    // Approach 1 : Using for loop from 0 to min(n1, n2)
+    // Approach 1 : Using for loop from 1 to min(n1, n2)
+    // int gcd = 1;
+    // for (int i = 1; i <= min(n1, n2); i++)
+    // {
+    //     if (n1 % i == 0 && n2 % i == 0)
+    //     {
+    //         gcd = i;
+    //     }
+    // }
+    // cout << "GCD : " << gcd << endl;
 
-    int gcd = 1;
-    for (int i = 1; i < min(n1, n2); i++)
+    // Approach 2 : Using for loop from min(n1, n2) to 1
+    int gcd;
+    for (int i = min(n1, n2); i > 0; i--)
     {
         if (n1 % i == 0 && n2 % i == 0)
         {
             gcd = i;
+            break;
         }
     }
     cout << "GCD : " << gcd << endl;
