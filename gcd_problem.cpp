34ed4833dd0a1a177f5f402cb1707f70bfbe5ee9
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-25 22:53:30 +0530

    day4

--- gcd_problem.cpp
+++ gcd_problem.cpp
@@ -7,9 +7,9 @@ int main()
 {
     int n1, n2;
     cin >> n1 >> n2;
+    int gcd;
 
     // Approach 1 : Using for loop from 1 to min(n1, n2)
-    // int gcd = 1;
     // for (int i = 1; i <= min(n1, n2); i++)
     // {
     //     if (n1 % i == 0 && n2 % i == 0)
@@ -20,16 +20,31 @@ int main()
     // cout << "GCD : " << gcd << endl;
 
     // Approach 2 : Using for loop from min(n1, n2) to 1
-    int gcd;
-    for (int i = min(n1, n2); i > 0; i--)
+    // for (int i = min(n1, n2); i > 0; i--)
+    // {
+    //     if (n1 % i == 0 && n2 % i == 0)
+    //     {
+    //         gcd = i;
+    //         break;
+    //     }
+    // }
+    // cout << "GCD : " << gcd << endl;
+
+    // Approach 3 : Using Euclidean Algorithm
+    int a = n1, b = n2;
+    while (a > 0 && b > 0)
     {
-        if (n1 % i == 0 && n2 % i == 0)
+        if (a > b)
         {
-            gcd = i;
-            break;
+            a = a - b;
+        }
+        else
+        {
+            b = b - a;
         }
     }
-    cout << "GCD : " << gcd << endl;
+    gcd = a + b;
 
+    cout << "GCD : " << gcd << endl;
     return 0;
 }
