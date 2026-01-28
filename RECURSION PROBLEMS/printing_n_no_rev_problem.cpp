703dce56c6c3112a9b0a50a8a3dc206a9461ca21
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-28 17:10:33 +0530

    day6\

---
+++ RECURSION PROBLEMS/printing_n_no_rev_problem.cpp
@@ -0,0 +1,23 @@
+// Problem Description: Given an integer N, write a program to print numbers from N to 1.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printN(int n)
+{
+    if (n < 1)
+    {
+        return;
+    }
+    // Approach 1 : Forward Recusion
+    cout << n << endl;
+    printN(n - 1);
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    printN(n);
+    return 0;
+}
