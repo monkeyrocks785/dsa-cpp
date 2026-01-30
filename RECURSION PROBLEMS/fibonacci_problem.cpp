96b5f318aa0fe3bd6555a272b456657b9902c0b0
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-30 20:26:21 +0530

    day7

--- RECURSION PROBLEMS/fibonacci_problem.cpp
+++ RECURSION PROBLEMS/fibonacci_problem.cpp
@@ -3,40 +3,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void fibonacci(int n)
+// void fibonacci(int n)
+// {
+// Approach:
+//     BruteForce / Iterative if (n < 1)
+//                      cout
+//         << n << " ";
+
+//     int fib[n + 1];
+//     fib[0] = 0;
+//     fib[1] = 1;
+//     for (int i = 2; i <= n; i++)
+//     {
+//         fib[i] = fib[i - 1] + fib[i - 2];
+//     }
+//     for (int i = 0; i <= n; i++)
+//     {
+//         cout << fib[i] << " ";
+//     }
+
+// Approach:
+//     Optimized Iterative int a = 0, b = 1;
+//     if (n == 0)
+//     {
+//         cout << a << " ";
+//     }
+//     else
+//     {
+//         cout << a << " " << b << " ";
+//         for (int i = 2; i <= n; i++)
+//         {
+//             int next = a + b;
+//             cout << next << " ";
+//             a = b;
+//             b = next;
+//         }
+//     }
+// }
+
+// Approach : Recursive
+int fibonacci(int n)
 {
-    // Approach: BruteForce/Iterative
-    // if (n < 1)
-    //     cout << n << " ";
-
-    // int fib[n + 1];
-    // fib[0] = 0;
-    // fib[1] = 1;
-    // for (int i = 2; i <= n; i++)
-    // {
-    //     fib[i] = fib[i - 1] + fib[i - 2];
-    // }
-    // for (int i = 0; i <= n; i++)
-    // {
-    //     cout << fib[i] << " ";
-    // }
-
-    // Approach: Optimized Iterative
-    int a = 0, b = 1;
-    if (n == 0)
+    if (n <= 1)
     {
-        cout << a << " ";
+        return n;
     }
     else
     {
-        cout << a << " " << b << " ";
-        for (int i = 2; i <= n; i++)
-        {
-            int next = a + b;
-            cout << next << " ";
-            a = b;
-            b = next;
-        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
 
@@ -44,6 +58,11 @@ int main()
 {
     int n;
     cin >> n;
-    fibonacci(n);
+    // fibonacci(n);  // for direct approaches
+    // For recursive Approach
+    for (int i = 0; i <= n; i++)
+    {
+        cout << fibonacci(i) << " ";
+    }
     return 0;
 }
