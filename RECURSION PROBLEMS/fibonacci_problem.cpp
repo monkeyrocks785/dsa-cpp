1c147a7019ecd2537afd28145ce11a300ecb6f67
monkeyrocks <mayankgarg785mi@gmail.com>
2026-01-30 17:33:07 +0530

    day7

--- RECURSION PROBLEMS/fibonacci_problem.cpp
+++ RECURSION PROBLEMS/fibonacci_problem.cpp
@@ -5,19 +5,38 @@ using namespace std;
 
 void fibonacci(int n)
 {
-    if (n < 1)
-        cout << n << " ";
+    // Approach: BruteForce/Iterative
+    // if (n < 1)
+    //     cout << n << " ";
 
-    int fib[n + 1];
-    fib[0] = 0;
-    fib[1] = 1;
-    for (int i = 2; i <= n; i++)
+    // int fib[n + 1];
+    // fib[0] = 0;
+    // fib[1] = 1;
+    // for (int i = 2; i <= n; i++)
+    // {
+    //     fib[i] = fib[i - 1] + fib[i - 2];
+    // }
+    // for (int i = 0; i <= n; i++)
+    // {
+    //     cout << fib[i] << " ";
+    // }
+
+    // Approach: Optimized Iterative
+    int a = 0, b = 1;
+    if (n == 0)
     {
-        fib[i] = fib[i - 1] + fib[i - 2];
+        cout << a << " ";
     }
-    for (int i = 0; i <= n; i++)
+    else
     {
-        cout << fib[i] << " ";
+        cout << a << " " << b << " ";
+        for (int i = 2; i <= n; i++)
+        {
+            int next = a + b;
+            cout << next << " ";
+            a = b;
+            b = next;
+        }
     }
 }
 
