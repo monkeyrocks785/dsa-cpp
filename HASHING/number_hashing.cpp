eba252e85a01fcf237e49b58ac8d6002316dd591
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-01 20:58:46 +0530

    day8

--- HASHING/number_hashing.cpp
+++ HASHING/number_hashing.cpp
@@ -4,31 +4,55 @@
 using namespace std;
 
 // Approach : BruteForce
-int bruteforce(int n, int a[], int u)
-{
-    int cnt = 0;
-    for (int i = 0; i < u; i++)
-    {
-        if (a[i] == n)
-        {
-            cnt += 1;
-        }
-    }
-    return cnt;
-}
+// int bruteforce(int n, int a[], int u)
+// {
+//     int cnt = 0;
+//     for (int i = 0; i < u; i++)
+//     {
+//         if (a[i] == n)
+//         {
+//             cnt += 1;
+//         }
+//     }
+//     return cnt;
+// }
+
+// Time complexity of this approach will be - O(n*q) where n is size of arr, and q is size of query array
 
 int main()
 {
-    int arr[5] = {0, 5, 1, 5, 52};
-    int query[3] = {1, 5, 10};
-    int m = sizeof(query) / sizeof(query[0]);
-    int u = sizeof(arr) / sizeof(arr[0]);
+    int m = 3;
+    int u = 5;
+    int arr[u] = {0, 5, 1, 5, 8};
+    int query[m] = {1, 5, 10};
 
-    for (int i = 0; i < m; i++)
+    // for (int i = 0; i < m; i++)
+    // {
+    //     int r = bruteforce(query[i], arr, u);
+    //     cout << "Appearence of " << query[i] << " is : " << r << endl;
+    // }
+
+    // Approach : Hashing
+    // Here, maximum element in an array is 8, so we should create the hash array of size 9
+
+    int hash[9] = {0};
+
+    // Pre-storing
+    for (int i = 0; i < u; i++)
     {
-        int r = bruteforce(query[i], arr, u);
-        cout << "Appearence of " << query[i] << " is : " << r << endl;
+        hash[arr[i]] += 1;
     }
 
+    // Just knowing what hash array looks like (just a debugging part to knw about the array no real use, we can just directly jump to fetching)
+    // for (int i : hash)
+    // {
+    //     cout << i << " ";
+    // }
+
+    // Fetching
+    for (int j = 0; j < m; j++)
+    {
+        cout << "Appearence of " << query[j] << " is : " << hash[query[j]] << endl;
+    }
     return 0;
 }
