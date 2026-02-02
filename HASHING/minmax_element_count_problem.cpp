1d14eb40577ac40df01c029a79daa7771e15ece1
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-02 22:06:49 +0530

    day9

--- HASHING/minmax_element_count_problem.cpp
+++ HASHING/minmax_element_count_problem.cpp
@@ -6,34 +6,64 @@ using namespace std;
 void freqCount(int arr[], int n)
 {
     // Approach : Brute Force
-    vector<bool> visited(n, false);
+    // vector<bool> visited(n, false);
+    // int minf = n;
+    // int maxf = 0;
+    // int mine = 0;
+    // int maxe = 0;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     if (visited[i] == true)
+    //     {
+    //         continue;
+    //     }
+    //     int cnt = 0;
+    //     for (int j = 0; j < n; j++)
+    //     {
+    //         if (arr[i] == arr[j])
+    //         {
+    //             visited[j] = true;
+    //             cnt++;
+    //         }
+    //     }
+    //     if (cnt > maxf)
+    //     {
+    //         maxe = arr[i];
+    //         maxf = cnt;
+    //     }
+    //     if (cnt < minf)
+    //     {
+    //         mine = arr[i];
+    //         minf = cnt;
+    //     }
+    // }
+    // cout << "Max Frequency is : " << maxf << " of element : " << maxe << endl;
+    // cout << "Min Frequency is : " << minf << " of element : " << mine << endl;
+
+    // Approach : Optimal
+    unordered_map<int, int> map;
+    for (int i = 0; i < n; i++)
+    {
+        map[arr[i]]++;
+    }
     int minf = n;
     int maxf = 0;
     int mine = 0;
     int maxe = 0;
-    for (int i = 0; i < n; i++)
+    for (auto it : map)
     {
-        if (visited[i] == true)
-        {
-            continue;
-        }
-        int cnt = 0;
-        for (int j = 0; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-            {
-                visited[j] = true;
-                cnt++;
-            }
-        }
+        int element = it.first;
+        int cnt = it.second;
+
         if (cnt > maxf)
         {
-            maxe = arr[i];
+            maxe = element;
             maxf = cnt;
         }
+
         if (cnt < minf)
         {
-            mine = arr[i];
+            mine = element;
             minf = cnt;
         }
     }
