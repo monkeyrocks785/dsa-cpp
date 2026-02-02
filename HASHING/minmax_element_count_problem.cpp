cccd0521a7af679db45d73560fdd877b435c74f7
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-02 21:43:25 +0530

    day9

---
+++ HASHING/minmax_element_count_problem.cpp
@@ -0,0 +1,58 @@
+// Problem Statement: Problem Statement: Given an array of size N. Find the highest and lowest frequency element.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void freqCount(int arr[], int n)
+{
+    // Approach : Brute Force
+    vector<bool> visited(n, false);
+    int minf = n;
+    int maxf = 0;
+    int mine = 0;
+    int maxe = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (visited[i] == true)
+        {
+            continue;
+        }
+        int cnt = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                visited[j] = true;
+                cnt++;
+            }
+        }
+        if (cnt > maxf)
+        {
+            maxe = arr[i];
+            maxf = cnt;
+        }
+        if (cnt < minf)
+        {
+            mine = arr[i];
+            minf = cnt;
+        }
+    }
+    cout << "Max Frequency is : " << maxf << " of element : " << maxe << endl;
+    cout << "Min Frequency is : " << minf << " of element : " << mine << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements in array : ";
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter the element : ";
+        cin >> arr[i];
+    }
+    freqCount(arr, n);
+
+    return 0;
+}
