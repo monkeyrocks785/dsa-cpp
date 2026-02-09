8cef63c5b6a695718052555ccedd5a39b1cd4c3b
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-09 22:16:06 +0530

    day13

--- ARRAYS/left_shift_problem.cpp
+++ ARRAYS/left_shift_problem.cpp
@@ -7,6 +7,18 @@ using namespace std;
 void leftShift(vector<int> &nums, int n)
 {
     // Approach 1 : Brute Force
+    // int temp[n];
+    // for (int i = 1; i < n; i++)
+    // {
+    //     temp[i - 1] = nums[i];
+    // }
+    // temp[n - 1] = nums[0];
+    // for (int i = 0; i < n; i++)
+    // {
+    //     nums[i] = temp[i];
+    // }
+
+    // Approach 2 : Optimal Force
     int temp = nums[0];
     for (int i = 1; i < n; i++)
     {
