4f873c854806d4e84d22d16e159268ce39a5e2a9
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-12 14:37:39 +0530

    day14

---
+++ ARRAYS/move_zero_to_end_problem.cpp
@@ -0,0 +1,47 @@
+// Problem Statement: You are given an array of integers, your task is to move all the zeros in the array to the end of the array and move non-negative integers to the front by maintaining their order.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> moveZeroToEnd(vector<int> &arr, int n)
+{
+    vector<int> temp(n, 0);
+    int index = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] != 0)
+        {
+            temp[index] = arr[i];
+            index++;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = temp[i];
+    }
+
+    return arr;
+}
+
+int main()
+{
+    vector<int> arr = {0, 1, 0, 3, 12, 0, 0, 5, 0, 5, 6, 874, 25};
+    int n = arr.size();
+
+    cout << "Original Array: ";
+    for (auto x : arr)
+    {
+        cout << x << " ";
+    }
+
+    moveZeroToEnd(arr, n);
+
+    cout << "\nArray after moving zeros to the end: ";
+    for (auto x : arr)
+    {
+        cout << x << " ";
+    }
+
+    return 0;
+}
