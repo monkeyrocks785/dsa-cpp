76d9e82b539b7d0b3877a660dfe3656b986c11e4
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-12 12:34:49 +0530

    day14

--- ARRAYS/k_shift_problem.cpp
+++ ARRAYS/k_shift_problem.cpp
@@ -3,56 +3,92 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void rightShift(vector<int> &nums, int k, int n)
+// Approach : Brute Force
+// void rightShift(vector<int> &nums, int k, int n)
+// {
+//     k %= n;
+
+//     if (n == 0 || k == 0)
+//     {
+//         return;
+//     }
+
+//     int temp[k];
+//     for (int i = n - k; i < n; i++)
+//     {
+//         temp[i - (n - k)] = nums[i];
+//     }
+
+//     for (int i = n - k - 1; i >= 0; i--)
+//     {
+//         nums[i + k] = nums[i];
+//     }
+
+//     for (int i = 0; i < k; i++)
+//     {
+//         nums[i] = temp[i];
+//     }
+// }
+
+// void leftShift(vector<int> &nums, int k, int n)
+// {
+//     k %= n;
+
+//     if (n == 0 || k == 0)
+//     {
+//         return;
+//     }
+
+//     int temp[k];
+//     for (int i = 0; i < k; i++)
+//     {
+//         temp[i] = nums[i];
+//     }
+
+//     for (int i = k; i < n; i++)
+//     {
+//         nums[i - k] = nums[i];
+//     }
+
+//     for (int i = 0; i < k; i++)
+//     {
+//         nums[n - k + i] = temp[i];
+//     }
+// }
+
+// Approach : Optimal
+void reverseArr(vector<int> &nums, int start, int end)
 {
-    k %= n;
-
-    if (n == 0 || k == 0)
+    while (start < end)
     {
-        return;
-    }
-
-    int temp[k];
-    for (int i = n - k; i < n; i++)
-    {
-        temp[i - (n - k)] = nums[i];
-    }
-
-    for (int i = n - k - 1; i >= 0; i--)
-    {
-        nums[i + k] = nums[i];
-    }
-
-    for (int i = 0; i < k; i++)
-    {
-        nums[i] = temp[i];
+        swap(nums[start], nums[end]);
+        start++;
+        end--;
     }
 }
 
-void leftShift(vector<int> &nums, int k, int n)
+vector<int> shiftArr(vector<int> &nums, int k, int n, string direction)
 {
     k %= n;
-
     if (n == 0 || k == 0)
     {
-        return;
+        return nums;
     }
 
-    int temp[k];
-    for (int i = 0; i < k; i++)
+    if (direction == "right")
     {
-        temp[i] = nums[i];
+        reverseArr(nums, 0, n - 1);
+        reverseArr(nums, 0, k - 1);
+        reverseArr(nums, k, n - 1);
+    }
+    if (direction == "left")
+    {
+        reverseArr(nums, 0, k - 1);
+        reverseArr(nums, k, n - 1);
+        reverseArr(nums, 0, n - 1);
     }
 
-    for (int i = k; i < n; i++)
-    {
-        nums[i - k] = nums[i];
-    }
-
-    for (int i = 0; i < k; i++)
-    {
-        nums[n - k + i] = temp[i];
-    }
+    return nums;
 }
 
 int main()
@@ -67,17 +103,19 @@ int main()
     }
 
     cout << "\nRotating array to the right by " << k << " elements: ";
-    rightShift(nums, k, n);
+    // rightShift(nums, k, n);
+    vector<int> rightArr = shiftArr(nums, k, n, "right");
     for (int i = 0; i < n; i++)
     {
-        cout << nums[i] << " ";
+        cout << rightArr[i] << " ";
     }
 
     cout << "\nRotating array to the left by " << k + 1 << " elements: ";
-    leftShift(nums, k + 1, n);
+    // leftShift(nums, k + 1, n);
+    vector<int> leftArr = shiftArr(nums, k + 1, n, "left");
     for (int i = 0; i < n; i++)
     {
-        cout << nums[i] << " ";
+        cout << leftArr[i] << " ";
     }
 
     return 0;
