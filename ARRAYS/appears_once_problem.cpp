98c94723b0979d526f6a2fd4434d466422d8aa80
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-22 12:16:50 +0530

    day18

---
+++ ARRAYS/appears_once_problem.cpp
@@ -0,0 +1,28 @@
+// Problem Statement: Given a non-empty array of integers arr, every element appears twice except for one. Find that single one.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNum(vector<int> &arr)
+{
+    int n = arr.size();
+    // Approach 1 : using linear search
+    for (int i = 0; i < n; i++)
+    {
+        int c = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+                c++;
+        }
+        if (c == 1)
+            return arr[i];
+    }
+}
+
+int main()
+{
+    vector<int> arr = {4, 1, 2, 1, 2};
+    cout << singleNum(arr) << endl;
+    return 0;
+}
