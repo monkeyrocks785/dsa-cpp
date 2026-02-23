5c08c8b814f9803514b1e124038e74f6483ce1a1
monkeyrocks <mayankgarg785mi@gmail.com>
2026-02-23 17:11:30 +0530

    day19

---
+++ ARRAYS/longest_subarr_of_given_sum_problem.cpp
@@ -0,0 +1,37 @@
+// Problem Statement: Given an array nums of size n and an integer k, find the length of the longest sub-array that sums to k. If no such sub-array exists, return 0.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarr(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int mx = 0;
+
+    // Approach 1: Brute Force
+    for (int si = 0; si < n; si++)
+    {
+        for (int ei = si; ei < n; ei++)
+        {
+            int s = 0;
+            for (int i = si; i <= ei; i++)
+            {
+                s += nums[i];
+            }
+            if (s == k)
+            {
+                mx = max(mx, ei - si + 1);
+            }
+        }
+    }
+    
+    return mx;
+}
+
+int main()
+{
+    vector<int> a = {-1, 1, 1, 2};
+    int k = 1;
+    cout << longestSubarr(a, k) << endl;
+    return 0;
+}
