deba5d56d722c0515ff8d694cde38e23c4b3ea78
monkeyrocks <mayankgarg785mi@gmail.com>
2026-04-02 17:48:58 +0530

    day22

--- ARRAYS/sorting012_arr_problem.cpp
+++ ARRAYS/sorting012_arr_problem.cpp
@@ -30,29 +30,50 @@ void sortZeroOneTwo(vector<int> &nums)
     //     nums[idx++] = 2;
     // }
 
-    // Approach : Optimal
-    int count0 = 0, count1 = 0, count2 = 0;
-    for (int x : nums)
-    {
-        if (x == 0)
-            count0++;
-        else if (x == 1)
-            count1++;
-        else
-            count2++;
-    }
+    // Approach : Better
+    // int count0 = 0, count1 = 0, count2 = 0;
+    // for (int x : nums)
+    // {
+    //     if (x == 0)
+    //         count0++;
+    //     else if (x == 1)
+    //         count1++;
+    //     else
+    //         count2++;
+    // }
 
-    for (int i = 0; i < count0; i++)
+    // for (int i = 0; i < count0; i++)
+    // {
+    //     nums[i] = 0;
+    // }
+    // for (int i = count0; i < count0 + count1; i++)
+    // {
+    //     nums[i] = 1;
+    // }
+    // for (int i = count0 + count1; i < count0 + count1 + count2; i++)
+    // {
+    //     nums[i] = 2;
+    // }
+
+    // Approach : Optimal (Dutch National Flag Algorithm)
+    int low = 0, mid = 0, high = nums.size() - 1;
+    while (mid <= high)
     {
-        nums[i] = 0;
-    }
-    for (int i = count0; i < count0 + count1; i++)
-    {
-        nums[i] = 1;
-    }
-    for (int i = count0 + count1; i < count0 + count1 + count2; i++)
-    {
-        nums[i] = 2;
+        if (nums[mid] == 0)
+        {
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
     }
 }
 
