e9c98554d47bf21b49fbf7769b84f0fd78c121e8
monkeyrocks <mayankgarg785mi@gmail.com>
2026-04-03 13:52:45 +0530

    day23

--- ARRAYS/rearrage_by_sign_problem.cpp
+++ ARRAYS/rearrage_by_sign_problem.cpp
@@ -6,30 +6,51 @@ using namespace std;
 vector<int> rearrangeBySign(vector<int> &A, int n)
 {
     // Approach : Brute Force
-    vector<int> pos;
-    vector<int> neg;
+    // vector<int> pos;
+    // vector<int> neg;
 
-    for (int x : A)
+    // for (int x : A)
+    // {
+    //     if (x > 0)
+    //         pos.push_back(x);
+    //     else
+    //         neg.push_back(x);
+    // }
+
+    // for (int i = 0; i < n / 2; i++)
+    // {
+    //     A[2 * i] = pos[i];
+    //     A[2 * i + 1] = neg[i];
+    // }
+
+    // return A;
+
+    // Approach : Optimal
+    int posIndex = 0;
+    int negIndex = 1;
+    vector<int> ans(n, 0);
+
+    for (int i = 0; i < n; i++)
     {
-        if (x > 0)
-            pos.push_back(x);
+        if (A[i] > 0)
+        {
+            ans[posIndex] = A[i];
+            posIndex += 2;
+        }
         else
-            neg.push_back(x);
+        {
+            ans[negIndex] = A[i];
+            negIndex += 2;
+        }
     }
 
-    for (int i = 0; i < n / 2; i++)
-    {
-        A[2 * i] = pos[i];
-        A[2 * i + 1] = neg[i];
-    }
-
-    return A;
+    return ans;
 }
 
 int main()
 {
-    int n = 4;
     vector<int> A{1, 2, -4, -5};
+    int n = A.size();
 
     vector<int> ans = rearrangeBySign(A, n);
 
